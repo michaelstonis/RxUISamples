ca827ad6baf0793c9805136b5c988c79c9fc11aa
Michael Stonis <michael.stonis@eightbot.com>
2019-06-23 18:53:59 -0500

    Setup of Blank Xamarin.Forms Shell Application

---
+++ RxUISamples/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using RxUISamples.Models;
+
+namespace RxUISamples.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
